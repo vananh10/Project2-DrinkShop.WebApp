dc51a569fe0753f0da3f0186195d4e2f41f74460
VanAnh <hinata130723@gmail.com>
2023-08-29 00:09:23 +0700

    Complete DB 1

---
+++ DrinkShop.Data/Configurations/AppConfigConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using DrinkShop.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DrinkShop.Data.Configurations
+{
+    public class AppConfigConfiguration : IEntityTypeConfiguration<AppConfig>
+    {
+        public void Configure(EntityTypeBuilder<AppConfig> builder)
+        {
+            builder.ToTable("AppConfigs");
+
+            builder.HasKey(x => x.Key);
+
+            builder.Property(x => x.Value).IsRequired(true);
+        }
+    }
+}
---
+++ DrinkShop.Data/Entities/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinkShop.Data.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public decimal OriginalPrice { get; set; }
+        public int Stock { get; set; }
+        public int ViewCount { get; set; }
+        public DateTime DateCreate { get; set;}
+        public string SeoAlias { get; set; }
+        public List<ProductInCategory> ProductInCategories { get; set; }
+
+        public List<OrderDetail> OrderDetails { get; set; }
+
+        public List<Cart> Carts { get; set; }
+
+        public List<ProductTranslation> ProductTranslations { get; set; }
+    }
+}
